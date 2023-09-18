ee358595fcf7cfc2ab82e1bf2ee3d15ecd8ec623
Joshua Lehmann <joshua.le1999@gmail.com>
2023-09-18 10:27:17 +0200

    #2: added pre commit hooks for prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'prettier'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'react/jsx-curly-brace-presence': ['warn', {
+            'props': 'always', 'children': 'ignore', 'propElementValues': 'always',
+        }],
+        'react/self-closing-comp': ['warn', {
+            'component': true, 'html': true,
+        }],
+        'no-console': ['warn', {
+            'allow': ['warn', 'info', 'error'],
+        }],
+        'no-unneeded-ternary': 'warn',
     },
 };
