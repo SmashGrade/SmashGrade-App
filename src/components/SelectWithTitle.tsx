9d125efa635b2bf0c59b5b2426e4f199cf271542
Jasmin Keller <jasmin.keller@hftm.ch>
2023-10-07 10:34:04 +0200

    fix PR reviews

--- src/components/SelectWithTitle.tsx
+++ src/components/SelectWithTitle.tsx
@@ -8,16 +8,14 @@ export interface SelectWithTitleProps {
 
 export default function SelectWithTitle({ title, selectProps }: SelectWithTitleProps) {
     return (
-        <div className={'select-with-title'}>
+        <div className={'select-with-title-container'}>
             <div className={'title'}>{title}</div>
-            <div>
-                <Select
-                    className={'select-element'}
-                    // This always sets the first value as default value, but can be overwritten by setting defaultValue inside selectProps
-                    defaultValue={selectProps?.options?.[0] ? selectProps?.options[0].value : null}
-                    {...selectProps}
-                />
-            </div>
+            <Select
+                className={'select-element'}
+                // This always sets the first value as default value, but can be overwritten by setting defaultValue inside selectProps
+                defaultValue={selectProps?.options?.[0] ? selectProps?.options[0].value : null}
+                {...selectProps}
+            />
         </div>
     );
 }
