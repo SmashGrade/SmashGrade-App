f91509fe485eff8a375cb7e73d9dec82e581720d
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-10-09 21:35:59 +0200

    #17: Small changes to fix typescript problem

--- src/features/overview/Onboarding.tsx
+++ src/features/overview/Onboarding.tsx
@@ -1,28 +1,29 @@
-import { useQuery } from '@tanstack/react-query';
-import styles from './Overview.module.scss';
-import { useState } from 'react';
 import SelectWithTitle from '@components/SelectWithTitle.tsx';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import styles from './Overview.module.scss';
 
 interface YearResponse {
-    year: number
+    year: number;
 }
 
 interface Year {
-    value: number,
-    label: string
+    value: number;
+    label: string;
 }
 
 interface CurriculumResponse {
-    id: number,
-    title: string,
-    year: number
+    id: number;
+    title: string;
+    year: number;
 }
 
 interface Curriculum {
-    value: string,
-    label: string,
-    year: number
+    value: string;
+    label: string;
+    year: number;
 }
+
 /*const startYears = [
     { value: 2023, label: '2023' },
     { value: 2024, label: '2024' },
@@ -38,58 +39,67 @@ interface Curriculum {
     { value: 'Lehrgang 4', label: 'Lehrgang 4', year: 2026 },
 ];*/
 
-async function getYears() : Promise<Year[]> {
-    return await fetch("http://localhost:3000/startYears")
-        .then( data => data.json() )
-        .then( (years : YearResponse[]) => {
-            const selectOptions : Year[] = [];
-            years.forEach(year => {
-                selectOptions.push( { value: year.year, label: year.year as string } )
-            })
+async function getYears(): Promise<Year[]> {
+    return await fetch('http://localhost:3000/startYears')
+        .then((data) => data.json())
+        .then((years: YearResponse[]) => {
+            const selectOptions: Year[] = [];
+            years.forEach((year) => {
+                selectOptions.push({ value: year.year, label: year.year.toString() });
+            });
 
             return selectOptions;
-        })
+        });
 }
 
-async function getCurriculums() : Promise<Curriculum[]> {
-    return await fetch("http://localhost:3000/curriculums")
-        .then( data => data.json() )
-        .then( (curriculums : CurriculumResponse[]) => {
-            const selectOptions : Curriculum[] = [];
-            curriculums.forEach(curriculum => {
-                selectOptions.push( { value: curriculum.title, label: curriculum.title, year: curriculum.year } )
-            })
+async function getCurriculums(): Promise<Curriculum[]> {
+    return await fetch('http://localhost:3000/curriculums')
+        .then((data) => data.json())
+        .then((curriculums: CurriculumResponse[]) => {
+            const selectOptions: Curriculum[] = [];
+            curriculums.forEach((curriculum) => {
+                selectOptions.push({ value: curriculum.title, label: curriculum.title, year: curriculum.year });
+            });
 
             return selectOptions;
-        })
+        });
 }
 
 export default function Onboarding() {
-    const yearQuery = useQuery({
+    const {
+        isLoading: yearsLoading,
+        error: yearsError,
+        data: yearData,
+    } = useQuery({
         queryKey: ['startYears'],
-        queryFn: getYears
+        queryFn: getYears,
     });
 
-    const curriculumQuery = useQuery({
+    const {
+        isLoading: curriculumsLoading,
+        error: curriculumsError,
+        data: curriculumsData,
+    } = useQuery({
         queryKey: ['curriculums'],
-        queryFn: getCurriculums
-    })
+        queryFn: getCurriculums,
+    });
 
-    const [currentYear, setCurrentYear] = useState(yearQuery.data && yearQuery.data.length > 0 ? yearQuery.data[0].value : 0);
+    const [currentYear, setCurrentYear] = useState(yearData?.length ? yearData[0].value : 0);
 
-    if( yearQuery.status === "loading" || curriculumQuery.status === "loading" ) {
+    if (yearsLoading || curriculumsLoading) {
         return <h2>Loading</h2>;
-
     }
-    if( yearQuery.status === "error" ) {
+    if (yearsError) {
         return <h2>Error while getting years</h2>;
     }
 
-    if( curriculumQuery.status === "error" ) {
-        return <h2>Error while getting curriculums</h2>;
+    if (curriculumsError) {
+        return <h2>Error while getting curriculums </h2>;
     }
 
-    const availableCurriculums = curriculumQuery.data.filter((curriculum) => curriculum.year === currentYear);
+    const availableCurriculums = curriculumsData?.length
+        ? curriculumsData.filter((curriculum) => curriculum.year === currentYear)
+        : [];
 
     return (
         <div className={styles.overviewContainer}>
@@ -97,7 +107,7 @@ export default function Onboarding() {
             <SelectWithTitle
                 key={'startYear'}
                 title={'Startjahr'}
-                selectProps={{ options: yearQuery.data, onChange: setCurrentYear }}
+                selectProps={{ options: yearData, onChange: setCurrentYear }}
             />
 
             <SelectWithTitle key={'curriculum'} title={'Lehrgang'} selectProps={{ options: availableCurriculums }} />
