2c454d8261859bfd553e6f63160df172fc08a3ac
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-10-09 16:05:43 +0200

    #17: added react-query

--- src/features/overview/Onboarding.tsx
+++ src/features/overview/Onboarding.tsx
@@ -1,26 +1,95 @@
+import { useQuery } from '@tanstack/react-query';
 import styles from './Overview.module.scss';
 import { useState } from 'react';
 import SelectWithTitle from '@components/SelectWithTitle.tsx';
 
-const startYears = [
+interface YearResponse {
+    year: number
+}
+
+interface Year {
+    value: number,
+    label: string
+}
+
+interface CurriculumResponse {
+    id: number,
+    title: string,
+    year: number
+}
+
+interface Curriculum {
+    value: string,
+    label: string,
+    year: number
+}
+/*const startYears = [
     { value: 2023, label: '2023' },
     { value: 2024, label: '2024' },
     { value: 2025, label: '2025' },
     { value: 2026, label: '2026' },
-];
+];*/
 
-const curriculums = [
+/*const curriculums = [
     { value: 'Lehrgang 0', label: 'Lehrgang 0', year: 2022 },
     { value: 'Lehrgang 1', label: 'Lehrgang 1', year: 2023 },
     { value: 'Lehrgang 2', label: 'Lehrgang 2', year: 2023 },
     { value: 'Lehrgang 3', label: 'Lehrgang 3', year: 2025 },
     { value: 'Lehrgang 4', label: 'Lehrgang 4', year: 2026 },
-];
+];*/
+
+async function getYears() : Promise<Year[]> {
+    return await fetch("http://localhost:3000/startYears")
+        .then( data => data.json() )
+        .then( (years : YearResponse[]) => {
+            const selectOptions : Year[] = [];
+            years.forEach(year => {
+                selectOptions.push( { value: year.year, label: year.year as string } )
+            })
+
+            return selectOptions;
+        })
+}
+
+async function getCurriculums() : Promise<Curriculum[]> {
+    return await fetch("http://localhost:3000/curriculums")
+        .then( data => data.json() )
+        .then( (curriculums : CurriculumResponse[]) => {
+            const selectOptions : Curriculum[] = [];
+            curriculums.forEach(curriculum => {
+                selectOptions.push( { value: curriculum.title, label: curriculum.title, year: curriculum.year } )
+            })
+
+            return selectOptions;
+        })
+}
 
 export default function Onboarding() {
-    const [currentYear, setCurrentYear] = useState(startYears[0].value);
+    const yearQuery = useQuery({
+        queryKey: ['startYears'],
+        queryFn: getYears
+    });
 
-    const availableCurriculums = curriculums.filter((curriculum) => curriculum.year === currentYear);
+    const curriculumQuery = useQuery({
+        queryKey: ['curriculums'],
+        queryFn: getCurriculums
+    })
+
+    const [currentYear, setCurrentYear] = useState(yearQuery.data && yearQuery.data.length > 0 ? yearQuery.data[0].value : 0);
+
+    if( yearQuery.status === "loading" || curriculumQuery.status === "loading" ) {
+        return <h2>Loading</h2>;
+
+    }
+    if( yearQuery.status === "error" ) {
+        return <h2>Error while getting years</h2>;
+    }
+
+    if( curriculumQuery.status === "error" ) {
+        return <h2>Error while getting curriculums</h2>;
+    }
+
+    const availableCurriculums = curriculumQuery.data.filter((curriculum) => curriculum.year === currentYear);
 
     return (
         <div className={styles.overviewContainer}>
@@ -28,7 +97,7 @@ export default function Onboarding() {
             <SelectWithTitle
                 key={'startYear'}
                 title={'Startjahr'}
-                selectProps={{ options: startYears, onChange: setCurrentYear }}
+                selectProps={{ options: yearQuery.data, onChange: setCurrentYear }}
             />
 
             <SelectWithTitle key={'curriculum'} title={'Lehrgang'} selectProps={{ options: availableCurriculums }} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ConfigProvider } from 'antd';
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
@@ -29,7 +33,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                 },
             }}
         >
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
         </ConfigProvider>
     </React.StrictMode>
 );
