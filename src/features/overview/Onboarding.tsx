e8fe8c91e2f0d00f7fe37514579df17198f71c67
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-10-11 14:21:31 +0200

    #17: removed unused comment

--- src/features/overview/Onboarding.tsx
+++ src/features/overview/Onboarding.tsx
@@ -25,21 +25,6 @@ interface Curriculum {
     year: number;
 }
 
-/*const startYears = [
-    { value: 2023, label: '2023' },
-    { value: 2024, label: '2024' },
-    { value: 2025, label: '2025' },
-    { value: 2026, label: '2026' },
-];*/
-
-/*const curriculums = [
-    { value: 'Lehrgang 0', label: 'Lehrgang 0', year: 2022 },
-    { value: 'Lehrgang 1', label: 'Lehrgang 1', year: 2023 },
-    { value: 'Lehrgang 2', label: 'Lehrgang 2', year: 2023 },
-    { value: 'Lehrgang 3', label: 'Lehrgang 3', year: 2025 },
-    { value: 'Lehrgang 4', label: 'Lehrgang 4', year: 2026 },
-];*/
-
 async function getYears(): Promise<Year[]> {
     const { data } = await axios.get<YearResponse[]>('http://localhost:3000/startYears');
 
