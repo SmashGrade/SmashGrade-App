cf2726fe7e6f8eb203cd75441a5558fbdba7ae75
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-10-17 18:51:38 +0200

    #12: removed unused component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Navigation from '@components/Navigation';
 import { Outlet } from '@tanstack/react-router';
 
 function App() {
     return (
         <>
-            <Navigation />
             <Outlet />
         </>
     );
