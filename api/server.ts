daf636d1375f3b54a671a89ae5e679a68006f28b
Joshua Lehmann <joshua.le1999@gmail.com>
2023-10-18 09:54:49 +0200

    Testing azure app service build with json server

---
+++ api/server.ts
@@ -0,0 +1,13 @@
+import * as jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('../db.json');
+const middlewares = jsonServer.defaults({ static: '.' });
+
+const port = process.env.PORT ?? 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+    console.log('JSON Server is running on port: ' + port);
+});
