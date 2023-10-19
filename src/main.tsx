b22b703eca18e58200462790aa8bbeba140dd78e
Joshua Lehmann <joshua.le1999@gmail.com>
2023-10-19 17:13:41 +0200

    Test lint staged

--- src/main.tsx
+++ src/main.tsx
@@ -9,31 +9,31 @@ import { ReactIntlProvider } from './i18n/ReactIntlProvider.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ReactIntlProvider>
-        <ConfigProvider
-            theme={{
-                components: {
-                    Collapse: {
-                        headerBg: colors.colorPrimary,
-                        colorTextHeading: colors.colorTextHeading,
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Collapse: {
+                            headerBg: colors.colorPrimary,
+                            colorTextHeading: colors.colorTextHeading,
+                        },
+                        Table: {
+                            headerBg: colors.colorPrimary,
+                            colorTextHeading: colors.colorTextHeading,
+                        },
                     },
-                    Table: {
-                        headerBg: colors.colorPrimary,
-                        colorTextHeading: colors.colorTextHeading,
+                    token: {
+                        colorPrimary: colors.colorPrimary,
+                        borderRadius: 12,
+                        colorBgContainer: colors.colorBgContainer,
+                        fontFamily: 'Roboto',
+                        colorLink: colors.colorLink,
+                        colorLinkHover: colors.colorLinkHover,
+                        colorLinkActive: colors.colorLinkActive,
                     },
-                },
-                token: {
-                    colorPrimary: colors.colorPrimary,
-                    borderRadius: 12,
-                    colorBgContainer: colors.colorBgContainer,
-                    fontFamily: 'Roboto',
-                    colorLink: colors.colorLink,
-                    colorLinkHover: colors.colorLinkHover,
-                    colorLinkActive: colors.colorLinkActive,
-                },
-            }}
-        >
-            <App />
-        </ConfigProvider>
+                }}
+            >
+                <App />
+            </ConfigProvider>
         </ReactIntlProvider>
     </React.StrictMode>
 );
