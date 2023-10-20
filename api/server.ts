7044355dfffc31ef4092108572b99d2c2f734159
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-10-20 11:05:07 +0200

    Removing logs

--- api/server.ts
+++ api/server.ts
@@ -9,5 +9,5 @@ const port = process.env.PORT ?? 3000;
 server.use(middlewares);
 server.use('/api', router);
 server.listen(port, () => {
-    console.log(`JSON Server is running on: ${port}/api}`);
+    console.info(`JSON Server is running on: ${port}/api}`);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { OnboardingPage } from './pages/OnboardingPage';
 
 function App() {
-    console.info('API URL', import.meta.env.VITE_BACKEND_API_URL);
     return <OnboardingPage />;
 }
 
