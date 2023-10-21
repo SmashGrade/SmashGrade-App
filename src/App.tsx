fad5384ea724d3e30dd35219e2847d4369454138
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-10-21 09:36:24 +0200

    #17: rebased

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { OnboardingPage } from './pages/OnboardingPage';
+import Navigation from '@components/Navigation';
+import { Outlet } from '@tanstack/react-router';
 
 function App() {
-    return <OnboardingPage />;
+    return (
+        <>
+            <Navigation />
+            <Outlet />
+        </>
+    );
 }
 
 export default App;
