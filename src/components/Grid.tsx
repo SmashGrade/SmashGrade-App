e0f60f5cf0e1dd7a061390a334e14eff8f36d3b2
Joshua Lehmann <joshua.le1999@gmail.com>
2023-10-26 16:21:50 +0200

    #60: Fix sonar issue

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,7 +1,7 @@
 import { AgGridReact } from 'ag-grid-react';
 import { AgGridReactProps } from 'ag-grid-react/lib/shared/interfaces';
 
-export default function Grid<TData>(props: AgGridReactProps<TData>) {
+export default function Grid<TData>(props: Readonly<AgGridReactProps<TData>>) {
     return (
         <div className={'ag-theme-alpine'} style={{ height: '100%' }}>
             <AgGridReact<TData> animateRows={true} {...props} />
