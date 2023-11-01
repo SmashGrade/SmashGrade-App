67768ec10aa13c8eda487c9f6ad0dc8cca590f18
Joshua Lehmann <joshua.le1999@gmail.com>
2023-11-01 16:36:45 +0100

    #42: Added some refactoring suggestions

--- src/features/student/modules/Modules.tsx
+++ src/features/student/modules/Modules.tsx
@@ -1,40 +1,28 @@
 import Course from '@features/student/modules/Course.tsx';
-import Rating, { RatingType } from '@features/student/modules/Rating.tsx';
-import { Collapse, CollapseProps, theme } from 'antd';
+import Rating from '@features/student/modules/Rating.tsx';
+import { Collapse } from 'antd';
+import { ItemType } from 'rc-collapse/es/interface';
 import { CSSProperties } from 'react';
-import styles from './Modules.module.scss';
+import colors from '../../../colors.module.scss';
 
-interface Props {
-    activeFilter: string;
-}
-
-interface ModuleCollapseProps extends CollapseProps {
+type ModuleCollapseItemProps = ItemType & {
     study: string;
-}
+};
 
-function getExtra(rating: number, ratingType: RatingType) {
-    return (
-        <div className={styles.extraContainer}>
-            <Rating rating={rating} isModule={true} ratingType={ratingType} />
-        </div>
-    );
-}
+const panelStyle: CSSProperties = {
+    marginBottom: 12,
+    fontWeight: 'bold',
+    background: colors.colorPrimary,
+    borderRadius: 12,
+    border: 'none',
+};
 
-function getContent() {
-    return (
-        <>
-            <Course name={'OOP1'} />
-            <Course name={'OOP2'} />
-        </>
-    );
-}
-
-const items: (panelStyle: CSSProperties) => ModuleCollapseProps['items'] = (panelStyle) => [
+const items: ModuleCollapseItemProps[] = [
     {
         key: '1',
         label: 'Mathematik',
         children: <p>{'Lorem ipsum dolor sit amet...'}</p>,
-        extra: getExtra(5.5, 'good'),
+        extra: <Rating rating={5.5} ratingType={'good'} />,
         style: panelStyle,
         study: 'Grundstudium',
     },
@@ -42,15 +30,20 @@ const items: (panelStyle: CSSProperties) => ModuleCollapseProps['items'] = (pane
         key: '2',
         label: 'Systemtechnik',
         children: <p>{'Lorem ipsum dolor sit amet...'}</p>,
-        extra: getExtra(2.3, 'bad'),
+        extra: <Rating rating={2.3} ratingType={'bad'} />,
         style: panelStyle,
         study: 'Fachstudium',
     },
     {
         key: '4',
         label: 'Programmieren',
-        children: getContent(),
-        extra: getExtra(5.0, 'good'),
+        children: (
+            <>
+                <Course name={'OOP1'} />
+                <Course name={'OOP2'} />
+            </>
+        ),
+        extra: <Rating rating={5.0} ratingType={'good'} />,
         style: panelStyle,
         study: 'Fachstudium',
     },
@@ -58,29 +51,17 @@ const items: (panelStyle: CSSProperties) => ModuleCollapseProps['items'] = (pane
         key: '3',
         label: 'Enterprise Technologien',
         children: <p>{'Lorem ipsum dolor sit amet...'}</p>,
-        extra: getExtra(4.9, 'median'),
+        extra: <Rating rating={4.9} ratingType={'median'} />,
         style: panelStyle,
         study: 'Schwerpunkt',
     },
 ];
 
-export default function Modules({ activeFilter }: Props) {
-    const { token } = theme.useToken();
+interface ModulesProps {
+    activeFilter: string;
+}
 
-    const panelStyle: React.CSSProperties = {
-        marginBottom: 12,
-        fontWeight: 'bold',
-        background: token.colorPrimary,
-        borderRadius: token.borderRadiusLG,
-        border: 'none',
-    };
-
-    const collapseItems = items(panelStyle)?.filter((item) => item.study == activeFilter);
-
-    return (
-        <>
-            {collapseItems && collapseItems.length > 0 && <Collapse items={collapseItems} />}
-            {!collapseItems || (collapseItems.length == 0 && <h2>No modules found</h2>)}
-        </>
-    );
+export default function Modules({ activeFilter }: ModulesProps) {
+    const collapseItems = items?.filter((item) => item.study == activeFilter);
+    return collapseItems?.length > 0 ? <Collapse items={collapseItems} /> : <h2>No modules found</h2>;
 }
--- src/features/student/modules/StudiesFilter.tsx
+++ src/features/student/modules/StudiesFilter.tsx
@@ -1,22 +1,20 @@
 import { Tabs, TabsProps } from 'antd';
 
-interface Props {
+interface StudiesFilterProps {
     defaultActiveKey: string;
     tabsProps: TabsProps;
 }
 
-export default function StudiesFilter({ tabsProps, defaultActiveKey }: Props) {
+export default function StudiesFilter({ tabsProps, defaultActiveKey }: StudiesFilterProps) {
     return (
-        <>
-            <Tabs
-                defaultActiveKey={defaultActiveKey}
-                items={[
-                    { key: 'Grundstudium', label: 'Grundstudium' },
-                    { key: 'Fachstudium', label: 'Fachstudium' },
-                    { key: 'Schwerpunkt', label: 'Schwerpunkt' },
-                ]}
-                {...tabsProps}
-            />
-        </>
+        <Tabs
+            defaultActiveKey={defaultActiveKey}
+            items={[
+                { key: 'Grundstudium', label: 'Grundstudium' },
+                { key: 'Fachstudium', label: 'Fachstudium' },
+                { key: 'Schwerpunkt', label: 'Schwerpunkt' },
+            ]}
+            {...tabsProps}
+        />
     );
 }
