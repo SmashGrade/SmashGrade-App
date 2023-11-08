e13be0c48d06040f9f7a670fab166454ee899c24
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-11-08 17:10:40 +0100

    #42: handle not existent rating

--- src/features/student/modules/Rating.tsx
+++ src/features/student/modules/Rating.tsx
@@ -1,15 +1,18 @@
 import styles from './Rating.module.scss';
 
-export type RatingType = 'good' | 'median' | 'bad';
+export type RatingType = 'good' | 'median' | 'bad' | 'none';
 
 const RATING_CLASSNAMES: Record<RatingType, string> = {
     good: styles.ratingGood,
     median: styles.ratingMedian,
     bad: styles.ratingBad,
+    none: styles.ratingNone,
 };
 
 const getRatingClass = (rating: number) => {
-    if (rating < 4) {
+    if (rating <= 0) {
+        return RATING_CLASSNAMES.none;
+    } else if (rating < 4) {
         return RATING_CLASSNAMES.bad;
     } else if (rating < 5) {
         return RATING_CLASSNAMES.median;
@@ -24,7 +27,7 @@ interface RatingProps {
 export default function Rating({ rating }: Readonly<RatingProps>) {
     return (
         <div className={`${styles.ratingContainer}`}>
-            <div className={`${styles.rating} ${getRatingClass(rating)}`}>{rating}</div>
+            <div className={`${styles.rating} ${getRatingClass(rating)}`}>{rating > 0 ? rating : '-'}</div>
         </div>
     );
 }
