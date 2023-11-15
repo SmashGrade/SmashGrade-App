b193749bb97d3a15033ee43389305617ee76c8b0
Joshua Lehmann <joshua.le1999@gmail.com>
2023-11-15 15:53:13 +0100

    FIX: Try to fix json server cors issue

--- api/server.ts
+++ api/server.ts
@@ -2,17 +2,18 @@ import * as jsonServer from 'json-server';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ logger: true, noCors: true });
 
 const port = process.env.PORT ?? 9000;
 
-server.use(middlewares);
-server.use(router);
 server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Accept', '*');
     next();
 });
+server.use(middlewares);
+server.use(router);
+
 server.listen(port, () => {
     console.info(`JSON Server API is running on port: ${port}`);
 });
