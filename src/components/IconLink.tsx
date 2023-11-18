add0db11b15913520296659309fbf74c6cd91eae
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-11-18 17:46:12 +0100

    #89: Made link to property typesafe and removed ts error

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -1,11 +1,11 @@
 import { MaterialIcon } from '@components/MaterialIcon.tsx';
-import { Link } from '@tanstack/react-router';
+import { Link, MakeLinkOptions } from '@tanstack/react-router';
 import { FormattedMessage } from 'react-intl';
 import { LocaleKey } from '../i18n/ReactIntlProvider.tsx';
 import styles from './IconLink.module.scss';
 
 interface IconLinkProps {
-    to?: string;
+    to?: MakeLinkOptions['to'];
     icon?: string;
     messageId?: LocaleKey;
     defaultMessage?: string;
@@ -14,7 +14,6 @@ interface IconLinkProps {
 
 export function IconLink({ to, icon, messageId, defaultMessage, description }: Readonly<IconLinkProps>) {
     return (
-        // @ts-expect-error params required but works without
         <Link to={to}>
             <div className={styles.menuItemIconAbove}>
                 {icon ? <MaterialIcon icon={icon} size={'large'} /> : null}
