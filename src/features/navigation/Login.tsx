ff07de8d63a5ed97c7b729a7a083807179344509
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-11-24 18:15:23 +0100

    #96: Remove set active account from login since it is added to app

--- src/features/navigation/Login.tsx
+++ src/features/navigation/Login.tsx
@@ -1,17 +1,9 @@
 import { InteractionType } from '@azure/msal-browser';
-import { useMsal, useMsalAuthentication } from '@azure/msal-react';
+import { useMsalAuthentication } from '@azure/msal-react';
 import { Alert } from 'antd';
-import { useEffect } from 'react';
 
 export function Login() {
-    const { instance } = useMsal();
-    const { result, error } = useMsalAuthentication(InteractionType.Redirect);
-
-    useEffect(() => {
-        if (result !== null && !instance.getActiveAccount() && instance.getAllAccounts().length > 0) {
-            instance.setActiveAccount(instance.getAllAccounts()[0]);
-        }
-    }, [instance, result]);
+    const { error } = useMsalAuthentication(InteractionType.Redirect);
 
     if (error) {
         return (
