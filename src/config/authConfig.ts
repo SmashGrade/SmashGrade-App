67492a5dcb7e6ee963353ec6f516c7fca3154fe5
Joshua Lehmann <joshua.le1999@gmail.com>
2023-11-25 08:45:05 +0100

    #96: add new scopes

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -52,7 +52,7 @@ export const msalConfig: Configuration = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest: RedirectRequest = {
-    scopes: ['User.Read'],
+    scopes: ['User.Read', 'User.Read.All'],
 };
 
 /**
