64aac480997ced2ed6051d6f51bbf5bba6161ac4
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-11-25 15:14:05 +0100

    #96: Type roles hook

--- src/hooks/useUserRoles.ts
+++ src/hooks/useUserRoles.ts
@@ -1,7 +1,13 @@
 import { useMsal } from '@azure/msal-react';
 
+export enum UserRoles {
+    Student = 'Student',
+    Teacher = 'Dozent',
+    CourseAdmin = 'KursAdmin',
+}
+
 export default function useUserRoles() {
     const instance = useMsal();
     const activeAccount = instance.accounts[0];
-    return activeAccount.idTokenClaims?.roles;
+    return activeAccount.idTokenClaims?.roles as UserRoles[] | undefined;
 }
