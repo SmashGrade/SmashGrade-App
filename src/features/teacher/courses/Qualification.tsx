14a6e8caca6830c1e81258c3e5847a1d3d93add0
leonardo.lang <leonardo.lang@hftm.ch>
2023-11-27 22:14:59 +0100

    feat: Improve Qualification to display data dynamically

--- src/features/teacher/courses/Qualification.tsx
+++ src/features/teacher/courses/Qualification.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Spin } from 'antd';
 
 import QualificationCard from '@features/teacher/courses/QualificationCard.tsx';
 import styles from '@pages/MyCoursePage.module.scss';
 
-interface qualification {
+interface Qualification {
     name: string;
     weight: string;
     rating: number;
@@ -14,41 +16,75 @@ interface QualificationProps {
     subheading: string;
 }
 
-export default function Qualification(props: QualificationProps) {
-    const [qualifications, setQualifications] = useState<qualification[]>([
-        {
-            name: 'Test 1',
-            weight: '20%',
-            rating: 3.5,
-        },
-        {
-            name: 'Test 2',
-            weight: '20%',
-            rating: 5.5,
-        },
-        {
-            name: 'Test 3',
-            weight: '60%',
-            rating: 4.5,
-        },
-    ]);
+interface AvgGrade {
+    id: number;
+    description: string;
+    grade: number;
+}
 
-    return (
-        <div className={`${styles.proofOfQualification} ${styles.background}`}>
-            <h3>{props.field}</h3>
-            <p>{props.subheading}</p>
-            <div className={styles.setWidth}>
-                {qualifications.map((qualification, index) => {
-                    return (
-                        <QualificationCard
-                            name={qualification.name}
-                            weight={qualification.weight}
-                            rating={qualification.rating}
-                            key={index}
-                        />
-                    );
-                })}
-            </div>
-        </div>
+interface Exam {
+    id: number;
+    description: string;
+    weight: number;
+    type: string;
+    avgGrades: AvgGrade[];
+}
+
+interface TeacherCourseResponse {
+    id: number;
+    version: number;
+    description: string;
+    number: string;
+    exams: Exam[];
+}
+
+const getCourses = async (activeFilter: string): Promise<Qualification[]> => {
+    const teacherCourseResponse = await axios.get<TeacherCourseResponse>(
+        `${import.meta.env.VITE_BACKEND_API_URL}/courseTeacher/${activeFilter}`
     );
+    return teacherCourseResponse.data.exams.map((exam) => ({
+        name: exam.description,
+        weight: exam.weight.toString(),
+        rating: exam.avgGrades[0].grade,
+    }));
+};
+
+export default function Qualification(props: QualificationProps) {
+    const {
+        isLoading: loading,
+        error: error,
+        data: data,
+    } = useQuery({
+        queryKey: [],
+        queryFn: () => getCourses(''),
+    });
+
+    if (loading) {
+        return <Spin />;
+    }
+
+    if (error) {
+        return <h2>Error</h2>;
+    }
+
+    if (data) {
+        return (
+            <div className={`${styles.proofOfQualification} ${styles.background}`}>
+                <h3>{props.field}</h3>
+                <p>{props.subheading}</p>
+                <div className={styles.setWidth}>
+                    {data.map((qualification, index) => {
+                        return (
+                            <QualificationCard
+                                name={qualification.name}
+                                weight={qualification.weight}
+                                rating={qualification.rating}
+                                key={index}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
