f6d2cdfc7970b2666d11990864e6a44cb6776533
Joshua Lehmann <joshua.le1999@gmail.com>
2023-11-30 16:13:06 +0100

    #96: Small improvements to Auth and profile picture

--- src/features/navigation/Login.tsx
+++ src/features/navigation/Login.tsx
@@ -1,22 +1,54 @@
-import { InteractionType } from '@azure/msal-browser';
-import { useMsalAuthentication } from '@azure/msal-react';
+import { InteractionRequiredAuthError, InteractionType } from '@azure/msal-browser';
+import { useMsal, useMsalAuthentication } from '@azure/msal-react';
 import { Alert } from 'antd';
+import { useEffect } from 'react';
 import { loginRequest } from '../../config/authConfig.ts';
+import styles from './Login.module.scss';
 
 export function Login() {
-    const { error } = useMsalAuthentication(InteractionType.Redirect, loginRequest);
+    const { instance, accounts } = useMsal();
+    const activeAccount = accounts[0];
+    const { login, error } = useMsalAuthentication(InteractionType.Silent, {
+        ...loginRequest,
+        loginHint: activeAccount?.idTokenClaims?.login_hint,
+    });
 
-    if (error) {
+    useEffect(() => {
+        if (error instanceof InteractionRequiredAuthError) {
+            login(InteractionType.Popup, loginRequest)
+                .then((response) => {
+                    if (!instance.getActiveAccount() && response?.account) {
+                        instance.setActiveAccount(response.account);
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }, [error, instance, login]);
+
+    if (error && !(error instanceof InteractionRequiredAuthError)) {
         return (
-            <Alert
-                message={'Login Error'}
-                description={`There was an error logging in: ${error.errorMessage} 
-                Please refresh the page and try again.`}
-                type={'error'}
-                closable
-            />
+            <div className={styles.loginContainer}>
+                <Alert
+                    message={'Login Error'}
+                    description={`There was an error logging in: ${error.errorMessage}
+                    Please refresh the page and try again.`}
+                    type={'error'}
+                    closable
+                />
+            </div>
         );
     }
 
-    return null;
+    return (
+        <div className={styles.loginContainer}>
+            <Alert
+                message={'Login in Progress'}
+                description={`You need to complete the Login PopUp before you can access the Site`}
+                type={'info'}
+                closable
+            />
+        </div>
+    );
 }
