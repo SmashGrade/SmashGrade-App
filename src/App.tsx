f5ceed9e8ac441fedc110fbfe2fb138c91985fda
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-12-01 23:17:17 +0100

    Updated msal dependencies to latest, made sure that there is always an active account set

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
 import { Login } from '@features/navigation/Login.tsx';
 import Navigation from '@features/navigation/Navigation.tsx';
 import { Outlet } from '@tanstack/react-router';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const TanStackRouterDevtoolsAsync =
     process.env.NODE_ENV === 'production'
@@ -15,6 +15,18 @@ const TanStackRouterDevtoolsAsync =
           );
 
 function App() {
+    const { accounts, instance } = useMsal();
+
+    useEffect(() => {
+        if (!instance.getActiveAccount() && accounts.length === 1) {
+            if (accounts[0].tenantId != import.meta.env.VITE_AUTH_AUTHORITY_ID) {
+                console.error('The tenant ID of the account does not match the tenant ID of the application.');
+                throw new Error('The tenant ID of the account does not match the tenant ID of the application.');
+            }
+            instance.setActiveAccount(accounts[0]);
+        }
+    }, [accounts, instance]);
+
     return (
         <>
             <AuthenticatedTemplate>
