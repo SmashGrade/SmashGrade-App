437c1ca9a9cb96bb65abc906f67be8520a18df57
Jasmin Keller <jasmin.keller@hftm.ch>
2023-12-08 10:23:39 +0100

    my modul placeholder added to settings

--- src/features/settings/Settings.tsx
+++ src/features/settings/Settings.tsx
@@ -1,8 +1,11 @@
-import { Card } from 'antd';
+import Onboarding from '@features/overview/Onboarding.tsx';
+import { UserProfile } from '@features/profile/UserProfile.tsx';
+import { Card, Select, Space, List } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import { useLocale } from '@hooks/useLocale.ts';
 import LocaleSwitcher from '../../i18n/LocaleSwitcher.tsx';
 import { FormattedMessage } from 'react-intl';
+import useUserRoles from '@hooks/useUserRoles.ts';
 
 import styles from './Settings.module.scss';
 
@@ -24,29 +27,38 @@ interface UserResponse {
 
 export default function Settings() {
     const { locale, setLocale } = useLocale();
+    const userRoles = useUserRoles();
+
     return (
         <div className={styles.settingsContainer}>
-            <Title level={1}>
-                <FormattedMessage
-                    id={'settings.title'}
-                    defaultMessage={'Einstellungen'}
-                    description={'Einstellungen Titel'}
-                />
-            </Title>
-            <Card size={'small'} loading={false} bordered={true} style={{ width: 350 }}>
-                <div className={styles.settingsPersonaRow}>
-                    <p>
-                        <b>Max Müller</b>
-                    </p>
-                    <p>Fachbereichsleiter:in</p>
-                </div>
-                <p>max.mueller@hftm.ch</p>
-            </Card>
-            <div className={styles.selectWithTitleContainer}>
-                <div className={styles.title}>Sprache</div>
+            <Space direction={'vertical'} size={'middle'} style={{ display: 'flex' }}>
+                <Title level={1}>
+                    <FormattedMessage
+                        id={'settings.title'}
+                        defaultMessage={'Einstellungen'}
+                        description={'Einstellungen Titel'}
+                    />
+                </Title>
+                <UserProfile />
+                <Card size={'small'} loading={false} bordered={true} style={{ width: 350 }}>
+                    <div className={styles.settingsPersonaRow}>
+                        <p>
+                            <b>Max Müller</b>
+                        </p>
+                        <p>{userRoles?.[0]}</p>
+                    </div>
+                    <p>max.mueller@hftm.ch</p>
+                </Card>
+                <div className={styles.selectWithTitleContainer}>
+                    <div className={styles.title}>Sprache</div>
 
-                <LocaleSwitcher setLocale={setLocale} locale={locale} />
-            </div>
+                    <LocaleSwitcher setLocale={setLocale} locale={locale} />
+                </div>
+                <Onboarding isReadonly={true} />
+                Meine Module
+                <Select />
+                <List />
+            </Space>
         </div>
     );
 }
