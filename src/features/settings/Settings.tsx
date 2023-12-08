df522bef46df9d3be23e35281026195d4cc393e3
Jasmin Keller <jasmin.keller@hftm.ch>
2023-12-08 11:24:06 +0100

    useUserProfile Hook added

--- src/features/settings/Settings.tsx
+++ src/features/settings/Settings.tsx
@@ -1,5 +1,4 @@
 import Onboarding from '@features/overview/Onboarding.tsx';
-import { UserProfile } from '@features/profile/UserProfile.tsx';
 import { Card, Select, Space, List } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import { useLocale } from '@hooks/useLocale.ts';
@@ -8,6 +7,8 @@ import { FormattedMessage } from 'react-intl';
 import useUserRoles from '@hooks/useUserRoles.ts';
 
 import styles from './Settings.module.scss';
+
+import { useUserProfile } from '@hooks/useUserProfile.ts';
 
 /*
 interface UserResponse {
@@ -28,26 +29,27 @@ interface UserResponse {
 export default function Settings() {
     const { locale, setLocale } = useLocale();
     const userRoles = useUserRoles();
+    const { userProfile, isLoading, error } = useUserProfile();
+
+    if (isLoading) return <p>Laden...</p>;
+    if (error) return <p>Fehler beim Laden des Benutzerprofils</p>;
 
     return (
         <div className={styles.settingsContainer}>
+            <Title level={1}>
+                <FormattedMessage
+                    id={'settings.title'}
+                    defaultMessage={'Einstellungen'}
+                    description={'Einstellungen Titel'}
+                />
+            </Title>
             <Space direction={'vertical'} size={'middle'} style={{ display: 'flex' }}>
-                <Title level={1}>
-                    <FormattedMessage
-                        id={'settings.title'}
-                        defaultMessage={'Einstellungen'}
-                        description={'Einstellungen Titel'}
-                    />
-                </Title>
-                <UserProfile />
                 <Card size={'small'} loading={false} bordered={true} style={{ width: 350 }}>
                     <div className={styles.settingsPersonaRow}>
-                        <p>
-                            <b>Max Müller</b>
-                        </p>
+                        <p>{userProfile?.displayName}</p>
                         <p>{userRoles?.[0]}</p>
                     </div>
-                    <p>max.mueller@hftm.ch</p>
+                    <p>max.placeholder@hftm.ch</p>
                 </Card>
                 <div className={styles.selectWithTitleContainer}>
                     <div className={styles.title}>Sprache</div>
