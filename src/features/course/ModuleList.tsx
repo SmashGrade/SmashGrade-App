4575686f36e6bc15156e5accc863856df715e6e7
wanjabachmann <wanja.bachmann@hotmail.com>
2023-12-28 01:54:13 +0100

    #111 Add menu per module for editing, removing and copying a module

--- src/features/course/ModuleList.tsx
+++ src/features/course/ModuleList.tsx
@@ -1,5 +1,4 @@
 import Grid from '@components/Grid.tsx';
-import { LinkButtonCellRenderer } from '@features/course/LinkButtonCellRenderer.tsx';
 import { newCourseRoute } from '@pages/routes/courseRoutes.ts';
 import { moduleRoute } from '@pages/routes/routes.ts';
 import { useQuery } from '@tanstack/react-query';
@@ -7,8 +6,10 @@ import { Link } from '@tanstack/react-router';
 import { ColDef } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-import { Button, Spin } from 'antd';
+import { Button, Spin, Dropdown } from 'antd';
+import type { MenuProps } from 'antd';
 import axios from 'axios';
+import { MoreOutlined } from '@ant-design/icons';
 import styles from './ModuleList.module.scss';
 
 export interface ModuleResponse {
@@ -21,16 +22,56 @@ export interface ModuleResponse {
     studyStage: StudyStage;
 }
 
+interface ModuleWithActions extends ModuleResponse {
+    actions?: 'Edit' | 'Copy' | 'Delete';
+}
+
 export interface StudyStage {
     id: number;
     description: string;
 }
 
+const menuItems: MenuProps['items'] = [
+    {
+        key: 'edit',
+        label: 'Editieren',
+    },
+    {
+        key: 'copy',
+        label: 'Kopieren',
+    },
+    {
+        key: 'delete',
+        label: 'Löschen',
+    },
+];
+
+const handleMenuClick = (action: string, id: number) => {
+    // handle the action here
+    switch (action) {
+        case 'edit':
+            // handle edit action
+            console.debug('edit' + id);
+            break;
+        case 'copy':
+            // handle edit action
+            console.debug('copy' + id);
+            break;
+        case 'delete':
+            // handle edit action
+            console.debug('delete' + id);
+            break;
+        // add cases for 'copy' and 'delete' if needed
+        default:
+            break;
+    }
+};
+
 const defaultModuleColDef: ColDef<ModuleResponse> = { sortable: true, filter: 'agTextColumnFilter' };
 
-const moduleColumnDefs: ColDef<ModuleResponse>[] = [
-    { field: 'id', headerName: '', filter: null, cellRenderer: LinkButtonCellRenderer },
-    { field: 'id', headerName: 'ID', filter: 'agNumberColumnFilter', sort: 'asc' },
+const moduleColumnDefs: ColDef<ModuleWithActions>[] = [
+    //{ field: 'id', headerName: '', filter: null, cellRenderer: LinkButtonCellRenderer },
+    //{ field: 'id', headerName: 'ID', filter: 'agNumberColumnFilter', sort: 'asc' },
     { field: 'description', headerName: 'Module', flex: 1 },
     { field: 'description', headerName: 'Studiengang', flex: 1 },
     { field: 'studyStage.description', headerName: 'Lehrgang' },
@@ -38,12 +79,30 @@ const moduleColumnDefs: ColDef<ModuleResponse>[] = [
     {
         field: 'isActive',
         headerName: 'Status',
+        cellStyle: { textAlign: 'center' },
         cellRenderer: (params: { value: boolean }) => {
             if (params.value) {
                 return <div className={styles.isActive}>Aktiv</div>;
             } else {
                 return <div className={styles.isInactive}>Inaktiv</div>;
             }
+        },
+    },
+    {
+        field: 'actions',
+        headerName: '',
+        resizable: false,
+        width: 50,
+        sortable: false,
+        filter: '',
+        cellRenderer: (params: { data: ModuleResponse }) => {
+            return (
+                <Dropdown menu={{ items: menuItems, onClick: ({ key }) => handleMenuClick(key, params.data.id) }}>
+                    <a onClick={(e) => e.preventDefault()}>
+                        <MoreOutlined />
+                    </a>
+                </Dropdown>
+            );
         },
     },
 ];
@@ -65,7 +124,6 @@ export default function ModuleList() {
             <Link from={moduleRoute.to} to={newCourseRoute.to}>
                 <Button type={'primary'}>+ New Module</Button>
             </Link>
-
             <Grid<ModuleResponse> columnDefs={moduleColumnDefs} rowData={data} defaultColDef={defaultModuleColDef} />
         </div>
     );
