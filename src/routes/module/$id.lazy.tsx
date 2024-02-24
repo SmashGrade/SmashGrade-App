3ffac862b7bbf7f3a704a375a0f3e5efce95b9c6
Joshua Lehmann <joshua.le1999@gmail.com>
2024-02-24 09:44:17 +0100

    #49: Revert prettier config

--- src/routes/module/$id.lazy.tsx
+++ src/routes/module/$id.lazy.tsx
@@ -2,5 +2,5 @@ import { EditModule } from '@features/course-admin/module/EditModule.tsx';
 import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/module/$id')({
-  component: EditModule,
+    component: EditModule,
 });
